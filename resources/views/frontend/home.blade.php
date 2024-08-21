de41d713c3ed4f83be8e046109757921eb7fff38
Surya Dinarta <Surya.10rpl1.2019@gmail.com>
2024-08-21 10:20:11 +0700

    setup

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,17 @@
+@extends('frontend.layouts.app')
+
+@section('meta')
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <title></title>
+@endsection
+
+@push('css')
+@endpush
+
+@section('content')
+@endsection
+
+@push('scripts')
+@endpush
