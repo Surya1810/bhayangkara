de31c8b264909e373505a1e0833f568606ea525e
surya1810 <surya.10rpl1.2019@gmail.com>
2025-07-16 14:32:51 +0700

    update copywriting

--- resources/views/frontend/kontak/index.blade.php
+++ resources/views/frontend/kontak/index.blade.php
@@ -9,6 +9,16 @@
 @section('content')
     <div class="container">
         <h2 class="text-center my-3"><strong>Kontak</strong></h2>
+        <div class="card">
+            <div class="card-body">
+                <h4>Alamat</h4>
+                <p>Jl. Tubagus Ismail VIII No.41 RT.002 RW.010, Kelurahan Sekeloa, Kecamatan Coblong, Kota Bandung</p>
+                <h4>Email</h4>
+                <p>hi.pwrib@gmail.com</p>
+                <h4>Telepon</h4>
+                <p>+62 821-2600-0126</p>
+            </div>
+        </div>
     </div>
 @endsection
 
--- resources/views/frontend/tentang/index.blade.php
+++ resources/views/frontend/tentang/index.blade.php
@@ -9,6 +9,22 @@
 @section('content')
     <div class="container">
         <h2 class="text-center my-3"><strong>Partner Bhayangkara</strong></h2>
+        <p>
+            Partner Bhayangkara adalah media digital yang fokus menyajikan informasi dan pemberitaan seputar Kepolisian
+            Negara Republik Indonesia (Polri). Berita yang kami sajikan mencakup aktivitas institusi kepolisian dari tingkat
+            pusat hingga daerah, kebijakan dan program kerja, tokoh Polri, kegiatan sosial, serta kolaborasi antara polisi
+            dan masyarakat.
+            <br>
+            Kami berdiri atas dasar objektivitas, netralitas, dan tanggung jawab sosial, guna membangun jembatan komunikasi
+            yang positif antara Polri dan masyarakat luas.
+        </p>
+        <h4>Misi</h4>
+        <ol>
+            <li>Menyampaikan berita kepolisian yang akurat dan terpercaya</li>
+            <li>Meningkatkan pemahaman masyarakat terhadap peran dan tugas Polri</li>
+            <li>Membangun citra humanis dan profesional Polri melalui narasi positif dan faktual</li>
+            <li>Menjadi media partner bagi satuan Polri dalam menyampaikan capaian dan kegiatan mereka</li>
+        </ol>
     </div>
 @endsection
 
